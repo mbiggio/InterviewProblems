9fb5758a0580f11612fb7fba4821c1dd8652bfa1
Matteo Biggio <matteo.biggio@amadeus.com>
2017-01-10 16:10:30 +0100

    find sequence in a matrix

---
+++ DynamicProgramming/seq_in_matrix.cpp
@@ -0,0 +1,111 @@
+#include <list>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+typedef enum {
+  STOP = 0,
+  NUL,
+  UP,
+  DOWN,
+  RIGHT,
+  LEFT
+} dir;
+
+bool check_neigh(vector<vector<vector<dir>>> &M,
+		 vector<int> &S,
+                 int i,
+                 int row,
+                 int col) {
+
+  int rows = M[0].size();
+  int cols = M[0][0].size();
+  if (i == 0) {
+    M[i][row][col] = STOP;
+  }
+  else if (row > 0      && M[i-1][row-1][col  ] != NUL) {
+    M[i][row][col] = UP;
+  }
+  else if (row < rows-1 && M[i-1][row+1][col  ] != NUL) {
+    M[i][row][col] = DOWN;
+  }
+  else if (col > 0      && M[i-1][row  ][col-1] != NUL) {
+    M[i][row][col] = LEFT;
+  }
+  else if (col < cols-1 && M[i-1][row  ][col+1] != NUL) {
+    M[i][row][col] = RIGHT;
+  }
+  else {
+    M[i][row][col] = NUL;
+  }
+}
+
+bool seq_in_matrix(vector<vector<int>> &A, vector<int> &S) {
+  int rows = A.size();
+  int cols = A[0].size();
+  int n = S.size();
+  list <pair<int,int>> l;
+  
+  /* initialize the memoization 3D matrix */
+  vector<vector<vector<dir>>> M(n);
+  for (int i = 0; i < n; i++) {
+    M[i] = vector<vector<dir>>(rows);
+    for (int j = 0; j < rows; j++) {
+      M[i][j] = vector<dir>(cols);
+    }
+  }
+
+  /* pseudo-recursion  */
+  for (int i = 0; i < n; i++)  {
+    bool true_found = false;       
+    for (int row = 0; row < rows; row++)  {
+      for (int col = 0; col < cols; col++)  {
+        if (A[row][col] == S[i]) {
+          check_neigh(M,S,i,row,col);
+          true_found = true_found || (M[i][row][col] != NUL);   
+        } else {
+          M[i][row][col] = NUL;
+        }
+      }
+    }
+    if (!true_found) return false;
+  }
+
+  return true;
+ 
+}
+
+void dump_input(vector<vector<int>> &A, vector<int> &S) {
+  cout << endl;
+  for (int i = 0; i < A.size(); i++) {
+    for (int j = 0; j < A[0].size(); j++) {
+      cout << A[i][j] << "  ";
+    }
+    cout << endl;
+  }
+  cout << endl;
+  for (int i = 0; i < S.size(); i++) {
+    cout << S[i] << "  ";
+  }
+  cout << endl << endl;
+}
+
+int main() {
+
+  int n,rows,cols;
+  cin >> n >> rows >> cols;  
+  vector<vector<int>>A(rows,vector<int>(cols));
+  vector<int> S(n);
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      cin >> A[i][j];
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    cin >> S[i];
+  }
+
+  dump_input(A,S);  
+  cout << seq_in_matrix(A,S) << endl;
+  return 0;
+}
